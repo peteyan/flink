15127f28befe7898d2afc1706e0cfab3bd13beb3
Pete <pete.yan@aliyun.com>
2020-10-22 18:59:22 +0800

    add datastream for kafka

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/kafka/KafkaDemo.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/kafka/KafkaDemo.java
@@ -1,20 +1,29 @@
 package org.apache.flink.streaming.examples.kafka;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
 
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.esotericsoftware.minlog.Log;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-
+/**
+ * filebeat -> kafka -> flink -> kafka.
+ */
 public class KafkaDemo {
+	private static final Logger LOG = LoggerFactory.getLogger(KafkaDemo.class);
+
 	public static void main(String[] args) throws Exception {
 		String inputTopic = "apache2-log";
 		String outputTopic = "apache2-log-output";
@@ -35,9 +44,9 @@ public class KafkaDemo {
 				StringDeserializer.class.getName());
 
 		// Define input consumer
-		FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>(
+		FlinkKafkaConsumer<ObjectNode> consumer = new FlinkKafkaConsumer<>(
 				inputTopic,
-				new SimpleStringSchema(),
+				new JSONKeyValueDeserializationSchema(false),
 				properties);
 
 		// Define output producer
@@ -47,10 +56,19 @@ public class KafkaDemo {
 				properties);
 
 		// Attach consumer to input data stream
-		DataStream<String> stringInputStream = environment.addSource(consumer);
+		DataStream<String> stringInputStream = environment
+				.addSource(consumer)
+				.map(new MapFunction<ObjectNode, String>() {
+					@Override
+					public String map(ObjectNode value) throws Exception {
+						Log.info(value.toPrettyString());
+						return value.get("value").get("message").asText();
+//						return "ping";
+					}
+				});
 
 		// Map process
-		DataStream<Tuple2<String, Integer>> mapper = stringInputStream
+		/*DataStream<Tuple2<String, Integer>> mapper = stringInputStream
 				.map(value -> new Tuple2<>(value, Integer.valueOf(1)))
 				.returns(Types.TUPLE(Types.STRING, Types.INT));
 
@@ -62,7 +80,8 @@ public class KafkaDemo {
 		DataStream<String> output = reducer.map(tuple -> tuple.f0 + " -> " + tuple.f1);
 
 		// Attach producer to output data stream
-		output.addSink(producer);
+		output.addSink(producer);*/
+		stringInputStream.addSink(producer);
 
 		environment.execute("Kafka Flink");
 	}
